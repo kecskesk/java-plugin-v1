c4a53e93410c760f006da7a8892a54dbc592e429
Krisztián Kecskés <kecskesk@sonrisa.hu>
2017-12-09 16:13:33 +0100

    tests for diamond operator

---
+++ test-diamond-anonym/src/main/java/bar/DiamondAnonymDemo.java
@@ -0,0 +1,37 @@
+package bar;
+
+public class DiamondAnonymDemo {
+	public abstract class TestClass<T> {
+		protected T myObject;
+		
+		public TestClass(T myObject) {
+			this.myObject = myObject;
+		}
+		
+		abstract String print();
+	}
+	
+	public void initTestClassWithDouble() {
+		TestClass<Double> integerTest = new TestClass<>(23d) {
+			String print() {
+				return myObject.toString();
+			}
+		};
+		System.out.println(integerTest.print());
+	}
+	
+	public void initTestClassWithInteger() {
+		TestClass<Integer> integerTest = new TestClass<Integer>(23) {
+			String print() {
+				return Integer.valueOf(myObject.hashCode()).toString();
+			}
+		};
+		System.out.println(integerTest.print());
+	}
+	
+	public static void main(String[] args) {
+		DiamondAnonymDemo demoApp = new DiamondAnonymDemo();
+		demoApp.initTestClassWithDouble();
+		demoApp.initTestClassWithInteger();
+	}
+}
